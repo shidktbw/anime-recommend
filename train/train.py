98a4a7ffef550dbec73b7e7a9f49791c9e6615e7
shidktbw <112849918+shidktbw@users.noreply.github.com>
2023-06-02 17:19:52 +0100

    Update train.py

--- train/train.py
+++ train/train.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Input, Embedding, Flatten, Dot, Dense
+from tensorflow.keras.layers import Input, Embedding, Flatten, Dense
 from tensorflow.keras.models import Model
 
 
@@ -19,15 +19,13 @@ train, test = train_test_split(df, test_size=0.2, random_state=42)
 
 studio_input = Input(shape=[1], name="Studio-Input")
 producer_input = Input(shape=[1], name="Producer-Input")
-type_input = Input(shape=[1], name="Type-Input")
 
 
 studio_embedding = Embedding(df['Studio'].nunique(), 5, name="Studio-Embedding")(studio_input)
 producer_embedding = Embedding(df['Producer'].nunique(), 5, name="Producer-Embedding")(producer_input)
-type_embedding = Embedding(df['Type'].nunique(), 5, name="Type-Embedding")(type_input)
 
 
-x = tf.keras.layers.concatenate([studio_embedding, producer_embedding, type_embedding])
+x = tf.keras.layers.concatenate([studio_embedding, producer_embedding])
 
 x = Flatten()(x)
 
@@ -36,21 +34,22 @@ x = Dense(256, activation='relu')(x)
 x = Dense(128, activation='relu')(x)
 x = Dense(64, activation='relu')(x)
 
-out = Dense(1, activation='relu')(x)
+out = Dense(df['Type'].nunique(), activation='softmax')(x)
 
+model = Model(inputs=[studio_input, producer_input], outputs=out)
 
-model = Model(inputs=[studio_input, producer_input, type_input], outputs=out)
-model.compile(loss='mean_squared_error', optimizer='adam')
+model.compile(loss='categorical_crossentropy', optimizer='adam')
 early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
-history = model.fit([train['Studio'], train['Producer'], train['Type']], train['Rating'], 
+# One-hot encode the target variable
+one_hot_encoder = OneHotEncoder()
+train_type = one_hot_encoder.fit_transform(train['Type'].values.reshape(-1, 1)).toarray()
+
+history = model.fit([train['Studio'], train['Producer']], train_type, 
                     batch_size=64, 
-                    epochs=100, # early stop = 10 step for this model
+                    epochs=100,
                     verbose=1,
                     validation_split=0.1,
                     callbacks=[early_stop])
 
-
-
 model.save('anime.h5')
-
